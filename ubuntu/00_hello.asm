d47f6c06471c47b34d2b045f495bae90ac8a58fb
Aleksandr Kardakov <razielsun@gmail.com>
2022-02-06 00:13:00 +0300

    first linux asm program

---
+++ ubuntu/00_hello.asm
@@ -0,0 +1,15 @@
+
+global _start
+
+section .text
+_start:	mov eax, 4 ; syscall write cmd
+	mov ebx, 1
+	mov ecx, message
+	mov edx, 13
+	int 80h
+	mov eax, 1 ; syscall exit cmd
+	mov ebx, 0 ; return 0
+	int 80h
+
+section .data
+message: db "Hello, World", 10
