00801013df9fea97b6ff3dd5c2eae44340a20512
Alexandr Kardakov <razielsun@gmail.com>
2022-02-17 08:36:07 +0300

    start adding strings

--- ubuntu/05_strings.asm
+++ ubuntu/05_strings.asm
@@ -3,9 +3,18 @@ global _start
 section .data
 
 section .bss
+buf resb 1024
 
 section .text
 _start:
+    xor al, al ; zero for eax lower bit
+    mov edi, buf ; di is destination index
+    mov ecx, 1024
+    cld ; clear set destination
+lp:
+    stosb   ; from al -> [edi] write data
+    loop lp
+
     mov eax, 1
     mov ebx, 0
     int 80h
